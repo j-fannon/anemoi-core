c7becccc36da928c150a8ca8571d92705dac1a57
j-fannon <91901684+j-fannon@users.noreply.github.com>
2026-05-21 10:26:32 +0000

    Make step 2 change to block.py

--- models/src/anemoi/models/layers/block.py
+++ models/src/anemoi/models/layers/block.py
@@ -502,8 +502,9 @@ class GraphTransformerBaseBlock(BaseBlock, ABC):
             self.k_norm = layer_kernels.KeyNorm(self.out_channels_conv)
 
         self.layer_norm_attention = LayerNorm(normalized_shape=in_channels)
-        self.layer_norm_mlp_dst = LayerNorm(normalized_shape=out_channels)
+        self.layer_norm_mlp = LayerNorm(normalized_shape=out_channels)
         self.node_dst_mlp = nn.Sequential(
+            self.layer_norm_mlp,
             Linear(out_channels, hidden_dim),
             Activation(),
             Linear(hidden_dim, out_channels),
@@ -537,7 +538,7 @@ class GraphTransformerBaseBlock(BaseBlock, ABC):
             self.conv = GraphTransformerConv(out_channels=self.out_channels_conv)
 
     def run_node_dst_mlp(self, x, **layer_kwargs):
-        return self.node_dst_mlp(self.layer_norm_mlp_dst(x, **layer_kwargs))
+        return self.node_dst_mlp(x, **layer_kwargs)
 
     def get_qkve(
         self,
